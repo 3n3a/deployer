f119fd5b62317f74d88b7dcee2701d7a901166de
3n3a <info@3n3a.ch>
2022-08-23 21:59:19 +0200

    add endpoint for deployer

--- src/index.js
+++ src/index.js
@@ -10,17 +10,49 @@ router.get('/', () => new Response(''))
 // POST to the collection (we'll use async here)
 router.post('/api/trigger', async request => {
   const apiKey = '' // X-API-KEY
+  if(!isApiKeyValid(apiKey)) return new Response('INVALID KEY')
+
   const body = await request.json()
   const part = body.part
-  
-  // trigger webhook
-  
-  if(isApiKeyValid(apiKey)) return new Response('OK')
-  return new Response('INVALID KEY')
+  const webhook_url = getWebhookUrl(part)
+  if(webhook_url) {
+    // triggers webhook
+    const res = await fetch(webhook_url, {body: '', method: 'POST'})
+    if(!res.ok) return new Response('INVALID WEBHOOK RESPONSE')
+
+    const json = await res.json()
+    console.log('webhook response', json)
+
+    return new Response('OK')
+  }
+    
+  return new Response('URL NOT FOUND')
 })
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }))
+
+/**
+ * Get Webhook Url by Name, or return false
+ * @param {string} part Name of Deploy Url
+ * @returns URL|boool
+ */
+function getWebhookUrl(part) {
+  const urls = JSON.parse(DEPLOY_URLS)
+  if(urls.hasOwnProperty(part)) return urls[part]
+  return false
+}
+
+/**
+ * Check if API Key is contained in Array
+ * @param {string} key Api key
+ * @returns bool
+ */
+function isApiKeyValid(key) {
+  const urls = JSON.parse(API_KEYS)
+  if(urls.includes(key)) return true
+  return false
+}
 
 // attach the router "handle" to the event handler
 addEventListener('fetch', event =>
